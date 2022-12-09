5c0f4e5f5ddb8076de8a7b28c0059eeff9c441b7
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2022-12-09 17:04:11 +0700

    Add WPF UI

--- FoodeLive/MainWindow.xaml.cs
+++ FoodeLive/MainWindow.xaml.cs
@@ -18,11 +18,12 @@ namespace FoodeLive
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
     }
 }
